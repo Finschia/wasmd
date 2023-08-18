b1343768c36d49fd541ff1277a8674934ee28b15
loloicci <loloicci@linecorp.com>
2023-08-18 19:58:09 +0900

    test: reflect comment and change how to assert events

--- x/wasm/keeper/msg_server_integration_test.go
+++ x/wasm/keeper/msg_server_integration_test.go
@@ -71,30 +71,71 @@ func TestUpdateAdmin(t *testing.T) {
 	var (
 		myAddress       sdk.AccAddress = make([]byte, types.ContractAddrLen)
 		_, _, otherAddr                = testdata.KeyTestPubAddr()
+		_, _, newAdmin                 = testdata.KeyTestPubAddr()
 	)
 
+	// setup
+	storeMsg := &types.MsgStoreCode{
+		Sender:                myAddress.String(),
+		WASMByteCode:          wasmContract,
+		InstantiatePermission: &types.AllowEverybody,
+	}
+	rsp, err := wasmApp.MsgServiceRouter().Handler(storeMsg)(ctx, storeMsg)
+	require.NoError(t, err)
+	var storeCodeResult types.MsgStoreCodeResponse
+	require.NoError(t, wasmApp.AppCodec().Unmarshal(rsp.Data, &storeCodeResult))
+	codeID := storeCodeResult.CodeID
+
+	initMsg := &types.MsgInstantiateContract{
+		Sender: myAddress.String(),
+		Admin:  myAddress.String(),
+		CodeID: codeID,
+		Label:  "test",
+		Msg:    []byte(`{}`),
+		Funds:  sdk.Coins{},
+	}
+	rsp, err = wasmApp.MsgServiceRouter().Handler(initMsg)(ctx, initMsg)
+	require.NoError(t, err)
+
+	var instantiateContractResult types.MsgInstantiateContractResponse
+	require.NoError(t, wasmApp.AppCodec().Unmarshal(rsp.Data, &instantiateContractResult))
+	contractAddress := instantiateContractResult.Address
+
 	specs := map[string]struct {
-		addr           string
-		expErr         bool
-		assertEventsFn func(*testing.T, []abci.Event, string, string)
+		addr      string
+		expErr    bool
+		expEvents []abci.Event
 	}{
 		"admin can update admin": {
 			addr:   myAddress.String(),
 			expErr: false,
-			assertEventsFn: func(t *testing.T, events []abci.Event, contractAddress, newAdmin string) {
-				assert.Equal(t, 2, len(events))
-				assert.Equal(t, "message", events[0].Type)
-				assert.Equal(t, 2, len(events[0].Attributes))
-				assert.Equal(t, "module", string(events[0].Attributes[0].Key))
-				assert.Equal(t, "wasm", string(events[0].Attributes[0].Value))
-				assert.Equal(t, "sender", string(events[0].Attributes[1].Key))
-				assert.Equal(t, myAddress.String(), string(events[0].Attributes[1].Value))
-				assert.Equal(t, "update_contract_admin", events[1].Type)
-				assert.Equal(t, 2, len(events[1].Attributes))
-				assert.Equal(t, "_contract_address", string(events[1].Attributes[0].Key))
-				assert.Equal(t, contractAddress, string(events[1].Attributes[0].Value))
-				assert.Equal(t, "new_admin_address", string(events[1].Attributes[1].Key))
-				assert.Equal(t, newAdmin, string(events[1].Attributes[1].Value))
+			expEvents: []abci.Event{
+				{
+					Type: "message",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("module"),
+							Value: []byte("wasm"),
+						},
+						{
+							Key:   []byte("sender"),
+							Value: []byte(myAddress.String()),
+						},
+					},
+				},
+				{
+					Type: "update_contract_admin",
+					Attributes: []abci.EventAttribute{
+						{
+							Key:   []byte("_contract_address"),
+							Value: []byte(contractAddress),
+						},
+						{
+							Key:   []byte("new_admin_address"),
+							Value: []byte(newAdmin.String()),
+						},
+					},
+				},
 			},
 		},
 		"other address cannot update admin": {
@@ -104,34 +145,7 @@ func TestUpdateAdmin(t *testing.T) {
 	}
 	for name, spec := range specs {
 		t.Run(name, func(t *testing.T) {
-			_, _, newAdmin := testdata.KeyTestPubAddr()
-
-			// setup
-			storeMsg := &types.MsgStoreCode{
-				Sender:                myAddress.String(),
-				WASMByteCode:          wasmContract,
-				InstantiatePermission: &types.AllowEverybody,
-			}
-			rsp, err := wasmApp.MsgServiceRouter().Handler(storeMsg)(ctx, storeMsg)
-			require.NoError(t, err)
-			var storeCodeResult types.MsgStoreCodeResponse
-			require.NoError(t, wasmApp.AppCodec().Unmarshal(rsp.Data, &storeCodeResult))
-			codeID := storeCodeResult.CodeID
-
-			initMsg := &types.MsgInstantiateContract{
-				Sender: myAddress.String(),
-				Admin:  myAddress.String(),
-				CodeID: codeID,
-				Label:  "test",
-				Msg:    []byte(`{}`),
-				Funds:  sdk.Coins{},
-			}
-			rsp, err = wasmApp.MsgServiceRouter().Handler(initMsg)(ctx, initMsg)
-			require.NoError(t, err)
-
-			var instantiateContractResult types.MsgInstantiateContractResponse
-			require.NoError(t, wasmApp.AppCodec().Unmarshal(rsp.Data, &instantiateContractResult))
-			contractAddress := instantiateContractResult.Address
+			xCtx, _ := ctx.CacheContext()
 
 			// when
 			msgUpdateAdmin := &types.MsgUpdateAdmin{
@@ -139,7 +153,7 @@ func TestUpdateAdmin(t *testing.T) {
 				NewAdmin: newAdmin.String(),
 				Contract: contractAddress,
 			}
-			rsp, err = wasmApp.MsgServiceRouter().Handler(msgUpdateAdmin)(ctx, msgUpdateAdmin)
+			rsp, err = wasmApp.MsgServiceRouter().Handler(msgUpdateAdmin)(xCtx, msgUpdateAdmin)
 
 			// then
 			if spec.expErr {
@@ -148,7 +162,7 @@ func TestUpdateAdmin(t *testing.T) {
 			}
 
 			require.NoError(t, err)
-			spec.assertEventsFn(t, rsp.Events, contractAddress, newAdmin.String())
+			assert.Equal(t, spec.expEvents, rsp.Events)
 		})
 	}
 }
