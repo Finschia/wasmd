5d0817f864336a07a80526cb371fa3aaf265fa71
kokeshiM0chi <frogandfrog009@gmail.com>
2023-08-24 17:22:29 +0900

    refactor: refactor TestInstantiateContract

--- x/wasm/keeper/msg_server_integration_test.go
+++ x/wasm/keeper/msg_server_integration_test.go
@@ -80,12 +80,39 @@ func TestInstantiateContract(t *testing.T) {
 	specs := map[string]struct {
 		addr       string
 		permission *types.AccessConfig
+		events     []abci.Event
 		expErr     bool
 	}{
 		"address can instantiate a contract when permission is everybody": {
 			addr:       myAddress.String(),
 			permission: &types.AllowEverybody,
-			expErr:     false,
+			events: []abci.Event{{
+				Type: "message",
+				Attributes: []abci.EventAttribute{{
+					Key:   []byte("module"),
+					Value: []byte("wasm"),
+					Index: false,
+				}, {
+					Key:   []byte("sender"),
+					Value: []byte(myAddress.String()),
+					Index: false,
+				},
+				},
+			}, {
+				Type: "instantiate",
+				Attributes: []abci.EventAttribute{{
+					Key:   []byte("_contract_address"),
+					Value: []byte("link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"),
+					Index: false,
+				}, {
+					Key:   []byte("code_id"),
+					Value: []byte("1"),
+					Index: false,
+				},
+				},
+			},
+			},
+			expErr: false,
 		},
 		"address cannot instantiate a contract when permission is nobody": {
 			addr:       myAddress.String(),
@@ -128,20 +155,8 @@ func TestInstantiateContract(t *testing.T) {
 			}
 
 			// check event
-			events := rsp.Events
-			assert.Equal(t, 2, len(events))
-			assert.Equal(t, "message", events[0].Type)
-			assert.Equal(t, 2, len(events[0].Attributes))
-			assert.Equal(t, "module", string(events[0].Attributes[0].Key))
-			assert.Equal(t, "wasm", string(events[0].Attributes[0].Value))
-			assert.Equal(t, "sender", string(events[0].Attributes[1].Key))
-			assert.Equal(t, myAddress.String(), string(events[0].Attributes[1].Value))
-			assert.Equal(t, "instantiate", events[1].Type)
-			assert.Equal(t, 2, len(events[1].Attributes))
-			assert.Equal(t, "_contract_address", string(events[1].Attributes[0].Key))
-			assert.Contains(t, string(rsp.Data), string(events[1].Attributes[0].Value))
-			assert.Equal(t, "code_id", string(events[1].Attributes[1].Key))
-			assert.Equal(t, "1", string(events[1].Attributes[1].Value))
+			assert.Equal(t, spec.events, rsp.Events)
+			assert.Contains(t, string(rsp.Data), string(rsp.Events[1].Attributes[0].Value))
 
 			require.NoError(t, err)
 		})
